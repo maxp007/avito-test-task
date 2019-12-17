66fbbcae507e8fabc4bcb7d97192fb35545466e1
max <vpupkin9208@gmail.com>
2019-12-17 22:14:15 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module avito-test-task
+
+go 1.13
+
+require (
+	github.com/maxp007/avito-test-task v0.0.0-20191217171526-05f9ec59effe // indirect
+	github.com/sirupsen/logrus v1.4.2
+)
--- main.go
+++ main.go
@@ -1,1 +1,34 @@
 package avito_test_task
+
+import (
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+	"sync"
+)
+
+const (
+	PORT    = 8080
+	IP_ADDR = "127.0.0.1"
+)
+
+func init() {
+	log.SetReportCaller(true)
+}
+
+func main() {
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		err := http.ListenAndServe(IP_ADDR+":"+fmt.Sprint(PORT), nil)
+		if err != nil {
+			log.Print("main service, fatal error: ", err)
+		}
+	}()
+
+	wg.Wait()
+	log.Print("Main microservice has stopped")
+}
